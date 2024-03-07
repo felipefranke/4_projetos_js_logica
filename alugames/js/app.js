862a67475c7de74420b6249cfb68947cebec369e
Felipe Franke <felipefranke@gmail.com>
2024-03-07 14:14:41 -0300

    Jogos alugados versão com desafios

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -40,14 +40,53 @@
 
 // versão otimizada
 
+// function alterarStatus(id) {
+//     let jogoClicado = document.getElementById(`game-${id}`);
+//     let imagem = jogoClicado.querySelector('.dashboard__item__img');
+//     let botao = jogoClicado.querySelector('.dashboard__item__button');
+
+//     if (jogoClicado) {
+//         imagem.classList.toggle('dashboard__item__img--rented');
+//         botao.classList.toggle('dashboard__item__button--return');
+//         botao.textContent = botao.classList.contains('dashboard__item__button--return') ? 'Devolver' : 'Alugar';
+//     }
+// }
+
+
+// Desafio :
+// 1 - No projeto Alugames, implemente uma confirmação de devolução.
+// 2 - No projeto Alugames, crie uma função para imprimir no console a informação sobre quantos jogos foram alugados.
+// 3 - Crie um programa que verifica se uma palavra ou frase é um palíndromo.
+// 4 - Crie um programa com uma função que receba três números como argumentos e os retorne em ordem crescente. Exiba os números ordenados.
+
+let jogosAlugados = 0
+
+function contagemdeJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`)
+}
+
 function alterarStatus(id) {
     let jogoClicado = document.getElementById(`game-${id}`);
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
     let botao = jogoClicado.querySelector('.dashboard__item__button');
+    let nomeDoJogo = jogoClicado.querySelector('.dashboard__item__name');
 
+    if (botao.classList.contains('dashboard__item__button--return')) {
+        alert(`${nomeDoJogo.textContent} devolvido com sucesso`)
+        jogosAlugados--;
+    } else {
+        alert(`${nomeDoJogo.textContent} alugado com sucesso, devolva quanto terminar de jogar para que mais pessoas possam aproveitar`)
+        jogosAlugados++;
+    }
     if (jogoClicado) {
         imagem.classList.toggle('dashboard__item__img--rented');
         botao.classList.toggle('dashboard__item__button--return');
         botao.textContent = botao.classList.contains('dashboard__item__button--return') ? 'Devolver' : 'Alugar';
     }
+    contagemdeJogosAlugados();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contagemdeJogosAlugados();
+})
