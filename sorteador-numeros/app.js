3721f11b3690f3b9ac6e7d355b7d7be4618c2e2b
Felipe Franke <felipefranke@gmail.com>
2024-03-07 06:37:03 -0300

    v1

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,6 @@
+function sortear() {
+    let quantidadeDeNumeros = document.getElementById('quantidade').value;
+    let numeroInicial = document.getElementById('de').value;
+    let numeroFinal = document.getElementById('ate').value;
+    alert(`Quantidade ${quantidadeDeNumeros}, de ${numeroInicial} até ${numeroFinal}`)
+}
