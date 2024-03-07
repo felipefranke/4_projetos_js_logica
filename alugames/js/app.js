a632dc57610a97f2aafbd34ddb31d37184131d15
Felipe Franke <felipefranke@gmail.com>
2024-03-07 13:40:36 -0300

    JS Aluguel de jogos v2

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,13 +1,39 @@
 
-let jogos = [document.getElementById('game-1'), document.getElementById('game-2'), document.getElementById('game-3')];
+// Minha solução
 
-function alterarStatus(posicaoArray) {
-    let jogo = jogos[posicaoArray - 1].querySelector('a');
-    let jogoImg = jogos[posicaoArray - 1].querySelector('.dashboard__item__img');
+// let jogos = [document.getElementById('game-1'), document.getElementById('game-2'), document.getElementById('game-3')];
 
-    if (jogo) {
-        jogo.classList.toggle('dashboard__item__button--return');
-        jogo.textContent = jogo.classList.contains('dashboard__item__button--return') ? 'Devolver' : 'Alugar';
-        jogoImg.classList.toggle('dashboard__item__img--rented');
+// function alterarStatus(posicaoArray) {
+//     let jogo = jogos[posicaoArray - 1].querySelector('a');
+//     let jogoImg = jogos[posicaoArray - 1].querySelector('.dashboard__item__img');
+
+//     if (jogo) {
+//         jogo.classList.toggle('dashboard__item__button--return');
+//         jogo.textContent = jogo.classList.contains('dashboard__item__button--return') ? 'Devolver' : 'Alugar';
+//         jogoImg.classList.toggle('dashboard__item__img--rented');
+//     }
+// }
+
+
+
+// versão dos instrutores
+
+function alterarStatus(id) {
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let botao = jogoClicado.querySelector('.dashboard__item__button');
+    // let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
+
+    // alert(nomeJogo.textContent);
+
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar'
+    }else {
+        imagem.classList.add('dashboard__item__img--rented')
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver'
     }
 }
